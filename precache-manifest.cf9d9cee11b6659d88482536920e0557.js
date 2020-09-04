4015c6d2f20d677d52d83f40effb3e6956a81afc
SunnyBarua <sunnybarua95@gmail.com>
2020-09-04 15:00:25 +0600

    Updates

---
+++ precache-manifest.cf9d9cee11b6659d88482536920e0557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7913f98056ccf229327234dce18e7a72",
+    "url": "/instaCloneByMern/index.html"
+  },
+  {
+    "revision": "3e0c8ce9961846b3f3ea",
+    "url": "/instaCloneByMern/static/css/main.297e71af.chunk.css"
+  },
+  {
+    "revision": "6a8ee374206d021bf4a2",
+    "url": "/instaCloneByMern/static/js/2.87dcc93c.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/instaCloneByMern/static/js/2.87dcc93c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0c8ce9961846b3f3ea",
+    "url": "/instaCloneByMern/static/js/main.53c786f3.chunk.js"
+  },
+  {
+    "revision": "5f7a46b6c786e3c0b2e5",
+    "url": "/instaCloneByMern/static/js/runtime-main.e47103d3.js"
+  }
+]);
